c42611b1c644d5cba30eaf94c36a201f04e2d07d
Zyverr <cybercelwinpopanes@gmail.com>
2022-09-06 20:10:14 +0800

    login

--- MainPOS/login.php
+++ MainPOS/login.php
@@ -2,8 +2,18 @@
 <html>
     <head>
         <title>User Login</title>
+        <link rel="stylesheet" type="text/css" href="loginstyles.css">
     </head>
     <body>
-        
+        <form action="index.php" method="POST">
+            <h2>Sandra Store Login</h2>
+            <label>User Name</label>
+            <input type="text" name="uname" placeholder="User Name">
+
+            <label>User Name</label>
+            <input type="password" name="password" placeholder="Password">
+
+            <button type="submit">Login</button>
+        </form>
     </body>
 </html>
