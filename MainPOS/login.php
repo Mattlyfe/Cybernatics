87562fa687b50fca416832a158530fec302ae3c4
Zyverr <cybercelwinpopanes@gmail.com>
2022-09-06 21:40:48 +0800

    Update login.php

--- MainPOS/login.php
+++ MainPOS/login.php
@@ -7,6 +7,9 @@
     <body>
         <form action="loginhandle.php" method="POST">
             <h2>Sandra Store Login</h2>
+            <?php if(isset($_GET['error'])) {?>
+                <p class="error"><?php echo $_GET['error']; ?></p>
+            <?php }?>
             <label>User Name</label>
             <input type="text" name="uname" placeholder="User Name">
 
