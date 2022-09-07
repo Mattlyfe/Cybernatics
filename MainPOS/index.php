f5e08055ff646ecd2c9042cd06d8a528f7c8add9
Zyverr <cybercelwinpopanes@gmail.com>
2022-09-07 21:10:20 +0800

    Update index.php

--- MainPOS/index.php
+++ MainPOS/index.php
@@ -3,6 +3,7 @@
         <title>Main POS</title>
         <link rel="stylesheet" href="styles.css">
         <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+        <script src="https://unpkg.com/boxicons@2.1.2/dist/boxicons.js"></script>
     </head>
     <body>
         <div class="wrapper">
@@ -19,7 +20,7 @@
                     <li><a href="#"><i class="fas fa-project-diagram"></i>Purchase Order</a></li>
                     <li><a href="#"><i class="fas fa-blog"></i>POS</a></li>
                     <li><a href="#"><i class="fas fa-address-book"></i>Online Orders</a></li>
-                    <li><a href="#"><i class="fas fa-map-pin"></i>Order History</a></li>
+                    <li><a href="#"><i class='bx bxs-time'></i>Order History</a></li>
                     <li><a href="#"><i class="fas fa-map-pin"></i>Sales Report</a></li>
                     <li><a href="#"><i class="fas fa-map-pin"></i>Messages</a></li>
                     <li><a href="#"><i class="fas fa-map-pin"></i>Account Management</a></li>
