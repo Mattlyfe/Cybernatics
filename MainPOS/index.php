a20669ef3bc7ee9e91c8b537cf53fabe09975024
Zyverr <cybercelwinpopanes@gmail.com>
2022-09-08 20:52:24 +0800

    update files

--- MainPOS/index.php
+++ MainPOS/index.php
@@ -2,7 +2,7 @@
     <head>
         <title>Main POS</title>
         <link rel="stylesheet" href="styles.css">
-        <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+        <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
         <script src="https://unpkg.com/boxicons@2.1.2/dist/boxicons.js"></script>
     </head>
     <body>
@@ -10,20 +10,16 @@
             <div class="sidebar">
                 <h2>Sandra's Store</span></h2>
                 <ul>
-                    <li>
-                        <a href="#"></i>Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="#"></i>User Registration</a>
-                    </li>
-                    <li><a href="#"><i class="fas fa-address-card"></i>Inventory</a></li>
-                    <li><a href="#"><i class="fas fa-project-diagram"></i>Purchase Order</a></li>
-                    <li><a href="#"><i class="fas fa-blog"></i>POS</a></li>
-                    <li><a href="#"><i class="fas fa-address-book"></i>Online Orders</a></li>
-                    <li><a href="#"><i class='bx bxs-time'></i>Order History</a></li>
-                    <li><a href="#"><i class="fas fa-map-pin"></i>Sales Report</a></li>
-                    <li><a href="#"><i class="fas fa-map-pin"></i>Messages</a></li>
-                    <li><a href="#"><i class="fas fa-map-pin"></i>Account Management</a></li>
+                    <li><a href="#"><i class='bx bx-home-circle' ></i>Dashboard</a></li>
+                    <li><a href="#"><i class='bx bxs-user-plus'></i>User Registration</a></li>
+                    <li><a href="#"><i class='bx bx-food-menu' ></i></i>Inventory</a></li>
+                    <li><a href="#"><i class='bx bx-purchase-tag-alt'></i></i>Purchase Order</a></li>
+                    <li><a href="#"><i class='bx bx-scan'></i></i>POS</a></li>
+                    <li><a href="#"><i class='bx bx-notepad'></i>Online Orders</a></li>
+                    <li><a href="#"><i class='bx bx-time' ></i></i>Order History</a></li>
+                    <li><a href="#"><i class='bx bxs-report'></i>Sales Report</a></li>
+                    <li><a href="#"><i class='bx bxs-chat' ></i>Messages</a></li>
+                    <li><a href="#"><i class='bx bxs-user-account'></i>Account Management</a></li>
                 </ul> 
             </div>
             <!--This section is for the main content of the current selected navigation-->
