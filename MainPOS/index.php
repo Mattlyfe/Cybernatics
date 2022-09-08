e4a10bab3a2afac8567f9a93d09ecad095931df3
Zyverr <cybercelwinpopanes@gmail.com>
2022-09-08 21:18:41 +0800

    Update index.php

--- MainPOS/index.php
+++ MainPOS/index.php
@@ -4,18 +4,17 @@
         <link rel="stylesheet" href="styles.css">
         <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
         <script src="https://unpkg.com/boxicons@2.1.2/dist/boxicons.js"></script>
-        <script src="/js/activepage.js"></script>
     </head>
     <body>
         <div class="wrapper">
             <div class="sidebar">
                 <h2>Sandra's Store</span></h2>
                 <ul>
-                    <li><a href="#"><i class='bx bx-home-circle' ></i>Dashboard</a></li>
+                    <li><a href="index.php"><i class='bx bx-home-circle' ></i>Dashboard</a></li>
                     <li><a href="#"><i class='bx bxs-user-plus'></i>User Registration</a></li>
                     <li><a href="#"><i class='bx bx-food-menu' ></i></i>Inventory</a></li>
-                    <li><a href="#"><i class='bx bx-purchase-tag-alt'></i></i>Purchase Order</a></li>
-                    <li><a href="#"><i class='bx bx-scan'></i></i>POS</a></li>
+                    <li><a href="purchaseorder.php"><i class='bx bx-purchase-tag-alt'></i></i>Purchase Order</a></li>
+                    <li><a href="pos.php"><i class='bx bx-scan'></i></i>POS</a></li>
                     <li><a href="#"><i class='bx bx-notepad'></i>Online Orders</a></li>
                     <li><a href="#"><i class='bx bx-time' ></i></i>Order History</a></li>
                     <li><a href="#"><i class='bx bxs-report'></i>Sales Report</a></li>
