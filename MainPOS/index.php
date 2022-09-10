9846c5b92c11273f03ab3d11bbe58378c8d6b9a5
Zyverr <cybercelwinpopanes@gmail.com>
2022-09-10 22:38:45 +0800

    widgets added (needs to be aligned)

--- MainPOS/index.php
+++ MainPOS/index.php
@@ -1,7 +1,9 @@
+<!DOCTYPE html>
 <html>
     <head>
         <title>Main POS</title>
         <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     </head>
     <body>
         <?php
@@ -9,24 +11,32 @@
         ?>
         <!--Main Content of the Dashboard-->
         <div class="main_content">
-                <!--To Be Shipped Widget-->
-                <div>
+            <!--To Be Shipped Widget-->
+                <div class="card-box bg-blue">
+                    <div class="inner">
+                        <h3> 13436 </h3>
+                        <p> Student Strength </p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                    </div>
+                    <a href="#" class="card-box-footer">View More <i class="fa fa-arrow-circle-right"></i></a>
                 </div>
-                <!--Status of Orders Widget-->
-                <div>
-                </div>
-                <!--Feedback Widget-->
-                <div>
-                </div>
-                <!--Stock Notification Widget-->
-                <div>
-                </div>
-                <!--Chart of Orders from Online and Onsite Widget-->
-                <div>
-                </div>
+            <!--Status of Orders Widget-->
+            <div class="">
+            </div>
+            <!--Feedback Widget-->
+            <div class="">
+            </div>
+            <!--Stock Notification Widget-->
+            <div class="">
+            </div>
+            <!--Chart of Orders from Online and Onsite Widget-->
+            <div class="">
+            </div>
             <!--Website Traffic Widget-->
             <div>
             </div>
-            </div>
+        </div>
     </body>
 </html>
