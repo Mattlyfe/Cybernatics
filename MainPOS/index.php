590b06e384fefd226f06474ee3b7fbd59d97d55d
Zyverr <cybercelwinpopanes@gmail.com>
2022-09-10 18:37:11 +0800

    .

--- MainPOS/index.php
+++ MainPOS/index.php
@@ -9,24 +9,24 @@
         ?>
         <!--Main Content of the Dashboard-->
         <div class="main_content">
-            <!--To Be Shipped Widget-->
-            <div>
-            </div>
-            <!--Status of Orders Widget-->
-            <div>
-            </div>
-            <!--Feedback Widget-->
-            <div>
-            </div>
-            <!--Stock Notification Widget-->
-            <div>
-            </div>
-            <!--Chart of Orders from Online and Onsite Widget-->
-            <div>
-            </div>
+                <!--To Be Shipped Widget-->
+                <div>
+                </div>
+                <!--Status of Orders Widget-->
+                <div>
+                </div>
+                <!--Feedback Widget-->
+                <div>
+                </div>
+                <!--Stock Notification Widget-->
+                <div>
+                </div>
+                <!--Chart of Orders from Online and Onsite Widget-->
+                <div>
+                </div>
             <!--Website Traffic Widget-->
             <div>
             </div>
-        </div>
+            </div>
     </body>
 </html>
