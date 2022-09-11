1dfedc9373dd57ec14119c0d2824fb85359b53d0
Zyverr <cybercelwinpopanes@gmail.com>
2022-09-11 20:10:17 +0800

    dashboard aligned

--- MainPOS/index.php
+++ MainPOS/index.php
@@ -3,7 +3,6 @@
     <head>
         <title>Main POS</title>
         <link rel="stylesheet" href="styles.css">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     </head>
     <body>
         <?php
@@ -12,25 +11,16 @@
         <!--Main Content of the Dashboard-->
         <div class="main_content">
             <!--To Be Shipped Widget-->
-                <div class="card-box bg-blue">
+                <div class="card-box bg-green">
+                <a href="#" class="card-box-footer"></a>
                     <div class="inner">
-                        <h3> 13436 </h3>
-                        <p> Student Strength </p>
+                        <h3> To Be Shipped Today </h3>
+                        <p> [placeholder] </p>
                     </div>
-                    <div class="icon">
-                        <i class="fa fa-graduation-cap" aria-hidden="true"></i>
-                    </div>
-                    <a href="#" class="card-box-footer">View More <i class="fa fa-arrow-circle-right"></i></a>
                 </div>
             <!--Status of Orders Widget-->
-            <div class="">
-            </div>
             <!--Feedback Widget-->
-            <div class="">
-            </div>
             <!--Stock Notification Widget-->
-            <div class="">
-            </div>
             <!--Chart of Orders from Online and Onsite Widget-->
             <div class="">
             </div>
