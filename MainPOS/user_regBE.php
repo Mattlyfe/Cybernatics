95bc8db076844601c466ade913de0cda0f6c1866
exCIeyeVi <mirandajohnlyod@gmail.com>
2022-09-12 20:11:10 +0800

    Update user_regBE.php

--- MainPOS/user_regBE.php
+++ MainPOS/user_regBE.php
@@ -2,6 +2,7 @@
 <html>
     <head>
     </head>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <body>
         <?php
             include("sidenav.php");
@@ -9,27 +10,32 @@
                 echo 'User submitted.';
             }
         ?>
-        <div>
+        <div class="main_content">
             <form action="user_regBE.php" method="post">
                 <div class="container">
-                    <h1>Registration</h1>
-                    <label for="first_name"><b>First Name</b></label>
-                    <input type="text" name="first_name" required>
                     
-                    <label for="last_name"><b>Last Name</b></label>
-                    <input type="text" name="last_name" required>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h1>Registration</h1>
+                        <hr class="mb-3">
+                        <label for="first_name"><b>First Name</b></label>
+                        <input class="form-control" type="text" name="first_name" required>
+                        
+                        <label for="last_name"><b>Last Name</b></label>
+                        <input class="form-control" type="text" name="last_name" required>
 
-                    <label><b>Password</b>
-                    <input type="password" name="pswd" id="pswd"  onkeyup='check();' />
-                    </label>
+                        <label><b>Password</b>
+                        <input class="form-control" type="password" name="pswd" id="pswd" required onkeyup='check();' />
+                        </label>
 
-                    <label><b>Confirm Password</b>
-                    <input type="password" name="cpswd" id="cpswd"  onkeyup='check();' /> 
-                    <span id='message'></span>
-                    </label>
-
-                    <input type="submit" name="sbmt_btn" value="Register">
-                    
+                        <label><b>Confirm Password</b>
+                        <input class="form-control" type="password" name="cpswd" id="cpswd" required onkeyup='check();' /> 
+                        <span id='message'></span>
+                        </label>
+                        <hr class="mb-3">
+                        <input class="btn btn-primary" type="submit" name="sbmt_btn" value="Register">
+                    </div>
+                    </div>
                     <script>
                     var check = function() {
                         if (document.getElementById('pswd').value ==
