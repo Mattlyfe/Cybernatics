871e2f9f27b30d0ee45b2bc0878a2abed7b46a5f
Zyverr <cybercelwinpopanes@gmail.com>
2022-09-13 19:41:18 +0800

    update on dashboard (not final)

--- MainPOS/index.php
+++ MainPOS/index.php
@@ -14,6 +14,15 @@
         <!--Main Content of the Dashboard-->
         <div class="main_content">
             <!--To Be Shipped Widget-->
+            <div class="card text-center">
+            <div class="card-header">
+                Status of Orders
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            </div>
             <!--Status of Orders Widget-->
             <div class="card text-center">
             <div class="card-header">
