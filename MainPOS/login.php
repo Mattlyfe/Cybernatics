cd177b6a30af62bb44a358d0332a7f45a3e62793
Zyverr <cybercelwinpopanes@gmail.com>
2022-09-16 21:11:53 +0800

    login updated

--- MainPOS/login.php
+++ MainPOS/login.php
@@ -3,20 +3,26 @@
     <head>
         <title>User Login</title>
         <link rel="stylesheet" type="text/css" href="loginstyles.css">
+        <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.2.0/css/all.css'>
     </head>
     <body>
         <form action="loginhandle.php" method="POST">
-            <h2>Sandra Store Login</h2>
+            <h2>Employee Login</h2>
             <?php if(isset($_GET['error'])) {?>
                 <p class="error"><?php echo $_GET['error']; ?></p>
             <?php }?>
-            <label>User Name</label>
-            <input type="text" name="uname" placeholder="User Name">
-
-            <label>User Name</label>
-            <input type="password" name="password" placeholder="Password">
-
-            <button type="submit">Login</button>
+            <div class="loginField">
+                <label>User Name</label>
+                <input type="text" name="uname" placeholder="User Name">
+            </div>
+            <div class="loginField">
+                <label>Password</label>
+                <input type="password" name="password" placeholder="Password">
+            </div>
+            <button type="submit" class="loginBtn">
+                    <span class="button__text">LOG IN </span>
+                    <i class="loginIcon fas fa-chevron-right"></i>
+            </button>	
         </form>
     </body>
 </html>
