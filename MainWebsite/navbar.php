a8582ed22e3ff1b1f8422f10ac07bd22fecaafd9
Zyverr <cybercelwinpopanes@gmail.com>
2022-09-23 21:51:18 +0800

    top navbar

---
+++ MainWebsite/navbar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="nbstyles.css">
+    </head>
+    <body>
+        <div class="container">
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Cart</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Login</li>
+                    <li>Signup</li>
+                    <li>Products</li>
+                </ul>
+            </nav>
+        </div>
+    </body>
+</html>
