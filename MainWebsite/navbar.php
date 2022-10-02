90f7d9f100618d8cb85944b6381087d0e02e7d46
Zyverr <cybercelwinpopanes@gmail.com>
2022-10-02 17:53:30 +0800

    navbar

---
+++ MainWebsite/navbar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Navbar Menu Hover Effect</title>
+    <link rel="stylesheet" href="navbarstyles.css">
+  </head>
+  <body>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Cart</a>
+      <a href="#">About Us</a>
+      <a href="#">Contact Us</a>
+      <a href="#">Login</a>
+      <a href="#">Signup</a>
+      <a href="#">Products</a>
+      <div class="animation start-home"></div>
+    </nav>
+  </body>
+</html>
