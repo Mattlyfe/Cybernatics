8d1bd38e88c04e2aa6934cdcf790006b351371ea
eyebols <charleslejarde@gmail.com>
2022-10-13 20:04:56 +0800

    tanggal category sidebar

--- MainWebsite/product-details.php
+++ MainWebsite/product-details.php
@@ -108,30 +108,6 @@ while ($rw=mysqli_fetch_array($ret)) {
 			<div class='col-md-3 sidebar'>
 				<div class="sidebar-module-container">
 
-
-					<!-- ==============================================CATEGORY============================================== -->
-<div class="sidebar-widget outer-bottom-xs wow fadeInUp">
-	<h3 class="section-title">Category</h3>
-	<div class="sidebar-widget-body m-t-10">
-		<div class="accordion">
-
-		            <?php $sql=mysqli_query($con,"select id,categoryName  from category");
-while($row=mysqli_fetch_array($sql))
-{
-    ?>
-	    	<div class="accordion-group">
-	            <div class="accordion-heading">
-	                <a href="category.php?cid=<?php echo $row['id'];?>"  class="accordion-toggle collapsed">
-	                   <?php echo $row['categoryName'];?>
-	                </a>
-	            </div>
-	          
-	        </div>
-	        <?php } ?>
-	    </div>
-	</div>
-</div>
-
 				</div>
 			</div><!-- /.sidebar -->
 <?php 
