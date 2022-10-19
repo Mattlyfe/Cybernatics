bf77d88a93b35387b1f78255f8f89bc25d2ac89f
eyebols <charleslejarde@gmail.com>
2022-10-19 18:59:23 +0800

    button&border

--- MainWebsite/category.php
+++ MainWebsite/category.php
@@ -181,11 +181,11 @@ while ($row=mysqli_fetch_array($ret))
 									<div class="action" style="color:red">Out of Stock</div>
 
 								<?php } else {?>
-									<button class="btn btn-primary icon" data-toggle="dropdown" type="button">
-								<i class="fa fa-shopping-cart"></i>		
-								<a href="category.php?page=product&action=add&id=<?php echo $row['id']; ?>">
-								<button class="btn btn-primary" type="button">Add to cart</button></a>											
-							</button>
+									<button class="btn btn-primary icon" data-toggle="dropdown" type="button">	
+									<a href="category.php?page=product&action=add&id=<?php echo $row['id']; ?>">
+									<i class="fa fa-shopping-cart"></i>Add to Cart
+								</button></a>															
+
 
 					<?php } ?>
 													
