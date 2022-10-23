9686e386e51726a49871023624f0263abaa08c65
exCIeyeVi <mirandajohnlyod@gmail.com>
2022-10-23 16:24:20 +0800

    Update login.php

--- MainWebsite/login.php
+++ MainWebsite/login.php
@@ -223,13 +223,13 @@ echo htmlentities($_SESSION['errmsg']="");
 
 <div class="form-group">
 	    	<label class="info-title" for="password">Password. <span>*</span></label>
-	    	<input type="password" class="form-control unicase-form-control text-input" id="password" name="password"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required >
+	    	<input type="password" class="form-control unicase-form-control text-input" id="password" name="password"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number, one uppercase and lowercase letter, and at least 8 or more characters" required >
 	  	</div>
 
 <div id="message">
 			<h3>Password must contain the following:</h3>
 			<p id="letter" class="invalid">A <b>lowercase</b> letter</p>
-			<p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
+			<p id="capital" class="invalid">An <b>uppercase</b> letter</p>
 			<p id="number" class="invalid">A <b>number</b></p>
 			<p id="length" class="invalid">Minimum <b>8 characters</b></p>
 		</div>
