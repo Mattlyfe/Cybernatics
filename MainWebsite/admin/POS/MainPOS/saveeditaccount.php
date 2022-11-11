e08b454f8a68449fd89dbd50946de8d80e4991f3
Mattlyfe <charlessanchez20@gmail.com>
2022-11-11 23:38:55 +0800

    Update saveeditaccount.php

--- MainWebsite/admin/POS/MainPOS/saveeditaccount.php
+++ MainWebsite/admin/POS/MainPOS/saveeditaccount.php
@@ -6,11 +6,12 @@ $id = $_POST['memi'];
 $fname = $_POST['first_name'];
 $lname = $_POST['last_name'];
 $pw = $_POST['password'];
-$sql = "UPDATE users_be 
-SET first_name=?, last_name=?, password=?,
-WHERE id=?";
-$q = $db->prepare($sql);
-$q->execute(array($fname,$lname,$pw,$id));
+
+    $sql = "UPDATE users_be 
+    SET first_name=?, last_name=?, password=?
+    WHERE ID=?";
+    $q = $db->prepare($sql);
+    $q->execute(array($fname,$lname,$pw,$id));
 
 header("location: accmngmnt.php");
 
