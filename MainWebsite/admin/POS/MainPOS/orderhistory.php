1c78c8b02c8ca862ef6cb56266c7d33d8602b375
Mattlyfe <charlessanchez20@gmail.com>
2022-11-12 15:38:03 +0800

    order history sorted by transid desc / fix order history ack end

--- MainWebsite/admin/POS/MainPOS/orderhistory.php
+++ MainWebsite/admin/POS/MainPOS/orderhistory.php
@@ -137,7 +137,7 @@ while($row=mysqli_fetch_array($query))
 
 				<tr>
 					<td class="cart-product-name-info"> IS-0<?php echo $tId = $row['transId'] ?></td>
-          <td class="cart-product-name-info"> ID<?php echo $uId = $row['uid'] ?> - <?php echo $row['uname'] ?></td>
+          <td class="cart-product-name-info"> Instore</td>
 
 					<td class="cart-product-name-info">
 						
