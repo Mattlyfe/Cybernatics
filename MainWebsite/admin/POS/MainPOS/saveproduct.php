1db42b49e64f78d86fc1d49c48cecc03c7200c53
Mattlyfe <charlessanchez20@gmail.com>
2022-12-04 18:19:33 +0800

    Update saveproduct.php

--- MainWebsite/admin/POS/MainPOS/saveproduct.php
+++ MainWebsite/admin/POS/MainPOS/saveproduct.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 include('connect.php');
+include('config.php');
+error_reporting(0);
+
 $a = $_POST['productCode'];
 $b = $_POST['genName'];
 $c = $_POST['productName'];
@@ -12,11 +15,20 @@ $h = $_POST['profit'];
 $i = $_POST['qty'];
 date_default_timezone_set('Asia/Taipei');
 $date = date("Y-m-d G:i:s");
-// query
-$sql = "INSERT INTO products (productCode,genName,productName,category,productPrice,productPriceBeforeDiscount,oPrice,profit,productAvailability, postingDate) VALUES (:a,:b,:c,:d,:f,:e,:g,:h,:i, :date)";
-$q = $db->prepare($sql);
-$q->execute(array(':a'=>$a,':b'=>$b,':c'=>$c,':d'=>$d,':f'=>$f,':e'=>$e,':g'=>$g,':h'=>$h,':i'=>$i, ':date'=>$date));
-header("location: inventory.php");
 
+$query=mysqli_query($con,"select * from products where productCode = '$a'");
+$row=mysqli_fetch_array($query);
+
+if($a != $row['productCode']){
+    // query
+    $sql = "INSERT INTO products (productCode,genName,productName,category,productPrice,productPriceBeforeDiscount,oPrice,profit,productAvailability, postingDate) VALUES (:a,:b,:c,:d,:f,:e,:g,:h,:i, :date)";
+    $q = $db->prepare($sql);
+    $q->execute(array(':a'=>$a,':b'=>$b,':c'=>$c,':d'=>$d,':f'=>$f,':e'=>$e,':g'=>$g,':h'=>$h,':i'=>$i, ':date'=>$date));
+    header("location: inventory.php");
+}
+else{
+    echo '<script>alert("Product Exists!");</script>';
+    echo '<script>window.location="inventory.php";</script>';
+}
 
 ?>
