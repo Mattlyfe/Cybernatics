5ea51e4f857ddfd5a3c83ec77a2a9a911ec12074
Zyverr <110810141+Zyverr@users.noreply.github.com>
2022-12-30 19:40:00 +0800

    RESIBO PRINT (DI PA TAPOS)

--- MainWebsite/admin/POS/MainPOS/showProdHistory.php
+++ MainWebsite/admin/POS/MainPOS/showProdHistory.php
@@ -13,6 +13,9 @@
 <link href="src/facebox.css" media="screen" rel="stylesheet" type="text/css" />
 <script src="lib/jquery.js" type="text/javascript"></script>
 <script src="src/facebox.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
+<script src="https://unpkg.com/jspdf-autotable@3.5.22/dist/jspdf.plugin.autotable.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
 
 <script type="text/javascript">
   jQuery(document).ready(function($) {
@@ -25,7 +28,7 @@
 <form name ="submit" method="post">
 <link href="../style.css" media="screen" rel="stylesheet" type="text/css" />
 <center><h4><i class="icon-edit icon-large"></i> Order # <?php echo$id?> Receipt</h4></center>
-<table class="table table-bordered">
+<table class="table table-bordered" >
 			<thead>
 				<tr>
 					<th class="cart-romove item">orderID</th>
@@ -115,4 +118,65 @@ while($row=mysqli_fetch_array($query1))
 				
 			</tbody><!-- /tbody -->
 		</table><!-- /table -->
-        <input type="submit" name="submit" value="Go back" class="btn btn-warning"></form>
+        <input type="submit" name="submit" value="Go back" class="btn btn-warning">
+		<button id="checkout" style="background: red;"type="button" class="btn btn-warning btn-lg">Print Receipt</button>
+	</form>
+	<script type="text/javascript">
+			function createPDF(){
+				var win = window.open('','','height=700,width=700');
+				win.print();   //PRINT CONTENTS.
+			}
+			$('#checkout').click(function(){
+				swal({
+					title: "Print Order Reciept?",
+					text: "Print the order receipt?",
+					icon: "warning",
+					dangermode: true,
+					closeOnClickOutside: false,
+                    buttons:["No","Yes"]
+				});
+				.then(willPrint => {
+					if (willPrint){
+						<?php
+						$getOrderId = mysqli_query($con, "SELECT DISTINCT products.productName as ")
+						?>
+						var doc = new jsPDF('p','pt','letter');
+						var body = [
+							['Item', 'Quantity', 'Price']
+						]
+						var y = 20;
+						doc.setLineWidth(1.5);
+						doc.setFont(undefined, 'bold').text(250, 50, "SANDRA'S STORE");
+						doc.text(170,70, "6017 Gen. T. De Leon, Valenzuela City");
+                        doc.text(155,90, "TIN #: SAMPLE TIN NUMBER");
+						doc.text(100,110, "Order Date: " +);
+						doc.text(100,130, "Order ID: " +);
+
+						doc.autoTable({
+							body: body,
+							startY: 150,
+							theme: 'plain',
+							headStyles:{halign:'center'},
+							columnStyles: {
+								0:{
+									halign: 'right',
+									tableWidth: 100,
+								},
+								1:{
+									halign: 'center',
+									tableWidth: 100,
+								},
+								2:{
+									halign: 'center',
+									tableWidth: 100,
+								}
+							}
+						})
+						doc.autoPrint();
+						window.open(doc.output('bloburl'))
+					}
+				})
+
+			});
+	</script>
+		
